7b9472b18d7246269c2ddf0268427329683df8d7
Mark Solante <marksolante221@gmail.com>
2025-01-19 11:30:18 +0800

    [UPDATE v0.1.5 : Page Controller Base]

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,17 @@
 // place files you want to import through the `$lib` alias in this folder.
+
+import { writable } from "svelte/store"
+import type { Page } from "./models/Page";
+
+
+const PageController = () => {
+    let currentPage = writable<Page>()
+    return {
+        current() {
+            return currentPage;
+        },
+        set(page : Page) {
+            currentPage.set(page)
+        }
+    }
+}
