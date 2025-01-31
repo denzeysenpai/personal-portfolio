214c523152ab49c296671bfcc93d3653673f8ec4
DenzeySenpai <marksolante221@gmail.com>
2025-01-31 18:51:27 +0800

    [UPDATE v0.2.2 : Movement Animation]

--- src/lib/components/GridBase/GridBase.svelte
+++ src/lib/components/GridBase/GridBase.svelte
@@ -13,11 +13,15 @@
   });
   $effect(() => {
     inView = Route.PositionViewTo(Route.Current ?? Routes.WelcomePage);
+    // TODO: add movement checker here
   });
 </script>
 
 {#if loadIn}
   <!-- THE IDEA IS TO NAVIGATE USING CSS HEHE -->
+   {#if Route.isMoving}
+    <div class="vignette"></div>
+   {/if}
   <div class="grid-base{' ' + inView}" id="grid-base-component">
     {#each pages as page}
       <div class="cell" id={page.Route}>
@@ -51,38 +55,47 @@
   .grid-base.wp {
     left: 0vw;
     top: 0vh;
+    transition: all 0.3s ease;
   }
   .grid-base.mpp {
     left: -100vw;
     top: 0vh;
+    transition: all 0.3s ease;
   }
   .grid-base.pop {
     left: -200vw;
     top: 0vh;
+    transition: all 0.3s ease;
   }
   .grid-base.ip {
     left: 0vw;
     top: -100vh;
+    transition: all 0.3s ease;
   }
   .grid-base.mp {
     left: -100vw;
     top: -100vh;
+    transition: all 0.3s ease;
   }
   .grid-base.ptp {
     left: -200vw;
     top: -100vh;
+    transition: all 0.3s ease;
   }
   .grid-base.op {
     left: 0vw;
     top: -200vh;
+    transition: all 0.3s ease;
   }
   .grid-base.cfp {
     left: -100vw;
     top: -200vh;
+    transition: all 0.3s ease;
   }
   .grid-base.ptp2 {
     left: -200vw;
     top: -200vh;
+    transition: all 0.3s ease;
   }
 
   @keyframes zoomOutAndIn {
@@ -97,19 +110,34 @@
     }
   }
 
-  .grid-base.cfp,
-  .grid-base.ip,
-  .grid-base.mp,
-  .grid-base.mpp,
-  .grid-base.op,
-  .grid-base.pop,
-  .grid-base.ptp,
-  .grid-base.ptp2,
-  .grid-base.wp {
-    /* animation-name: zoomOutAndIn;
-    animation-duration: 0.5s;
-    animation-iteration-count: 1;
-    animation-direction: normal;
-    animation-play-state: running; */
+  .grid-base.cfp.active,
+  .grid-base.ip.active,
+  .grid-base.mp.active,
+  .grid-base.mpp.active,
+  .grid-base.op.active,
+  .grid-base.pop.active,
+  .grid-base.ptp.active,
+  .grid-base.ptp2.active,
+  .grid-base.wp.active {
+    transform: scale(0.8);
+    transition: all 0.3s ease;
   }
+  
+  .vignette {
+    opacity: 0;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: 100;
+    box-shadow: 0 0 200px rgba(0,0,0,0.9) inset;
+    transition: all 0.3s ease;
+    
+  }
+  .vignette.active {
+    opacity: 1;
+    transition: all 0.3s ease;
+  }
+
 </style>
